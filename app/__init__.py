a08541407bab43f041d693c5349e3daab1322b59
Antje Kazimiers <antje@allesjetzt.net>
2018-05-30 10:31:59 -0400

    Add login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 import json
 from flask import Flask
 from flask_admin import Admin
+from flask_login import LoginManager
 
 app = Flask(__name__) #app variable, an object of class FLask
-
 from app import views
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 @app.context_processor
 def get_app_version():
