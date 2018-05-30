de7a85da785aafaa7bdc02f2a6e8315044f65068
Antje Kazimiers <antje@allesjetzt.net>
2018-05-30 09:12:27 -0400

    Add result page

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from app.settings import Settings
 @app.route('/', methods=['GET','POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/result', methods=['GET','POST'])
+def result():
+    return render_template('result.html')
