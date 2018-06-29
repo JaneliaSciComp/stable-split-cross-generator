aff989bfadae348f2e092d08ffb59df0d1cc58dd
Antje Kazimiers <antje@allesjetzt.net>
2018-06-29 14:43:09 -0400

    Add download

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,12 @@
 import ipdb, pika, sys, os, subprocess
-from flask import render_template, Flask, Response, redirect, url_for, request, session, abort
+from flask import render_template, Flask, Response, redirect, url_for, request, session, abort, send_from_directory
 from celery import Celery
 from myapp import myapp
 from myapp.settings import Settings
 from myapp.forms import LoginForm, RegisterForm
 from flask_login import LoginManager
 from flask_pika import Pika as FPika
+from pymongo import MongoClient
 
 login_manager = LoginManager()
 login_manager.init_app(myapp)
@@ -13,24 +14,44 @@ login_manager.login_view = "login"
 
 myapp.config.from_pyfile('sscg-config.cfg')
 celery = Celery('task', backend='amqp', broker='amqp://guest:guest@localhost:5672/')
-# celery = Celery(app.name, broker=app.config)
-# celery.conf.update(app.config)
+
+# Mongo client
+mongosettings = 'mongodb://' + myapp.config['MONGODB_SETTINGS']['host'] + ':' + str(myapp.config['MONGODB_SETTINGS']['port']) + '/'
+client = MongoClient(mongosettings)
 
 @celery.task
-def compute_splits_task(lines, a_line):
+def compute_splits_task(linenames, aline):
     ecosystem_path = myapp.config['IMAGING_ECOSYSTEM']
     if not ecosystem_path.startswith('/'): # relative
         ecosystem_path = os.path.join(myapp.root_path, ecosystem_path)
 
     gen1_split_generator = os.path.join(ecosystem_path, 'gen1_split_generator.py')
     # Call R, allow Rprofile.site file
-    if not lines:
-        cmd = "python {bin} --file line-names.txt".format(**{
+    # printf "55D12\n60b11\n40c01" | ./gen1_split_generator.py --deb
+    cmd = None
+    if not linenames:
+        cmd = "python {bin} --file line-names.txt --debug".format(**{
             'bin': gen1_split_generator
         })
+    else:
+        if aline:
+            cmd = "printf {linenames} | {bin} --aline {aline} --debug".format(**{
+                'linenames': linenames,
+                'bin': gen1_split_generator,
+                'aline': aline
+            })
+        else:
+            cmd = "printf {linenames} | {bin} --debug".format(**{
+                'linenames': linenames,
+                'bin': gen1_split_generator
+            })
+    print('command ' + cmd)
     pipe = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, encoding='utf8')
     stdout, stderr = pipe.communicate()
+    print('stdout {0}'.format(stdout))
+    print('stderr {0}'.format(stderr))
     if stderr: # TODO check exit code, only raise exception when not 0
+        print('An exception ocurred')
         raise Exception(stderr)
 
 
@@ -50,7 +71,21 @@ def result():
         obj.write(request.json['lnames'])
         obj.close()
         compute_splits_task.delay(None,None)
-    return render_template('result.html')
+
+    file1 = '58443-customName.crosses.txt'
+    file2 = '58443-customName.flycore.xls'
+    file3 = '58443-customName.no_crosses.txt'
+    return render_template('result.html', file1=file1, file2=file2, file3=file3)
+
+@myapp.route("/download/<file>")
+def download(file):
+    myfile = os.path.join(os.path.dirname(myapp.root_path), file)
+    print('myfile ' + myfile)
+    return send_from_directory(
+            os.path.dirname(myapp.root_path),
+            file,
+            as_attachment=True
+        )
 
 @myapp.route('/login', methods=['POST'])
 def login():
@@ -85,13 +120,14 @@ def register():
     flash('User successfully registered')
     return redirect(url_for('login'))
 
-@myapp.route('/compute_splits' , methods=['GET','POST'])
-def compute_splits():
-    lines = "lines"
-    a_line  = 'a_line'
-    task = compute_splits_task.delay(lines, a_line)
-    print(dir(task))
-    return 'task queued'
+@myapp.route('/compute_splits/<linenames>/<aline>' , methods=['GET','POST'])
+def compute_splits(linenames,aline):
+    print(linenames)
+    print(aline)
+    if linenames:
+        task = compute_splits_task.delay(linenames, aline)
+    # print(dir(task))
+    return redirect(url_for('result'))
 
 @login_manager.user_loader
 def load_user(id):
